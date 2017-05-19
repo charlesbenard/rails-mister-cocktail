7b751702867d518f88fc7d2478f46019c42935a0
Charles Benard <charles1benard@gmail.com>
2017-05-19 11:17:21 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cockails#index'
+
   resources :cocktails do
     resources :doses, only: [ :index, :show, :new, :create, :destroy ]
   end
-
-  root to: 'cockails#index'
 end
